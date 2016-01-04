9d8b72ff24284d3facc511bdcde5e0105b4969f2
Will <will118@gmail.com>
2016-01-04 21:33:45 +0000

    Protects speakerstate call, cleans up submodules.

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ function activewindowtitle()
   local win = window.focusedwindow()
   local colors = {32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97}
   if win then
-    local windowtitle = constanttextansigradient(win:title(), colors, 68)
+    local windowtitle = constanttextansigradient(win:title(), colors, 80)
     local apptitle = win:application():title()
     return '\27[30m' .. apptitle .. '\27[31m ፨ ' .. windowtitle
   else
@@ -45,6 +45,15 @@ function activewindowtitle()
 end
 
 function speakerstate()
+  local ok, value = pcall(speakerinfo)
+  if ok then
+    return value
+  else
+    return '\27[90m' .. 'HDMI'
+  end
+end
+
+function speakerinfo()
   local device = audiodevice.defaultoutputdevice()
   local volume = math.floor(device:volume())
   if device:muted() then
