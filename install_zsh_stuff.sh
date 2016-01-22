b3754166ec8de986e438a90f118c3fb091b410dd
Will Bartlett <will118@gmail.com>
2016-01-22 21:43:51 +0000

    Fixes mkdir error in zsh install script.

--- install_zsh_stuff.sh
+++ install_zsh_stuff.sh
@@ -1,4 +1,4 @@
-mkdir ~/.dots/.zfunctions
+mkdir ~/.zfunctions
 ln -sf ~/.dots/pure/pure.zsh "$HOME/.zfunctions/prompt_pure_setup"
 ln -sf ~/.dots/pure/async.zsh "$HOME/.zfunctions/async"
 ln -sf ~/.dots/completion.zsh "$HOME/.zfunctions/completion"
