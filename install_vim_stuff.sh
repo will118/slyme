a7737c5fe8e14f7ffa4d769d4d0ba76b8256f6bb
Will <will118@gmail.com>
2016-06-03 15:39:38 +0100

    Adds to vim install script a symlink of vimrc

--- install_vim_stuff.sh
+++ install_vim_stuff.sh
@@ -1,2 +1,4 @@
 mkdir -p ~/.vim/colors
 ln -sf "$HOME/.dots/base16-vim/colors/base16-ashes.vim" "$HOME/.vim/colors/base16-ashes.vim"
+mv ~/.vimrc ~/.vimrc.old
+ln -s "$HOME/.dots/.vimrc" "$HOME/.vimrc"
